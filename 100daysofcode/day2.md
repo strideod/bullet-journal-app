7563347523bde49a72b25b4c265cff7113ed0767
Mike Strid <mike.strid@protonmail.com>
2024-08-03 19:35:12 -0400

    day 2 commit

---
+++ 100daysofcode/day2.md
@@ -0,0 +1,11 @@
+# Day 2
+
+## Refactor directories
+
+Setup directories to more aling with a small appliction instead of a much larger project with multiple applications.
+
+## Tutorial
+
+Working through a tutorial to get a better understanding of FastAPI. Working thorugh Learning FastAPI the Hardway [FastAPI: Connecting to Database](https://www.fastapitutorial.com/blog/database-connection-fastapi/).
+
+Working through a couple tutorials will help get some knowledge to start working on the Bullet Journal application.
--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,5 +1,17 @@
-router = APIRouter()
+from services.user_service import UserService
+from db.session import get_db
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import Request
+from fastapi import responses
+from fastapi import status
+from fastapi.templating import Jinja2Templates
+from schemas.users im
 
-@router.post
+router = APIRouter
 
-@router.get
+
+@router.get("/register/")
+def register(request: Request):
+    return template.TemplateResponse("user/register.html", {"request": request})
+
